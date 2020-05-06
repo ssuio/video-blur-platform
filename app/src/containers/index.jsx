698b9d38ae4793e5fdf251ddd017b87647d51158
Ssuio <xssuio@gmail.com>
2020-05-07 00:44:50 +0800

    Update css

--- app/src/containers/index.jsx
+++ app/src/containers/index.jsx
@@ -7,12 +7,8 @@ import {
   Link,
   Redirect,
 } from "react-router-dom";
-import VedioView from "./Vedio";
 import { Login, Register, Logout } from "./User";
-import Dashboard from "./Dashboard";
 import Entry from "./Entry";
-import Upload from './Upload';
-import Pannel from './Pannel'
 import apiHelper from "../services/api";
 
 const Page404 = () => <h1> Page 404 </h1>;
@@ -57,33 +53,14 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
 
 function App() {
   return (
-    <div>
-      <div className="App">
+    <div className="App">
         <Router>
           <Switch>
             <Route path="/entry" component={Entry} />
             <PrivateRoute path="/dashboard" component={Logout} />
-
-            {/* <Route path="/dashboard/pannel" component={
-              ()=> {return (
-                <Dashboard>
-                  <Pannel />
-                </Dashboard>
-              )}
-            } />
-
-            <Route path="/dashboard/upload" component={
-              ()=> {return (
-                <Dashboard>
-                  <Upload />
-                </Dashboard>
-              )}
-            } /> */}
-
             <Route path="*" exact={true} component={Page404} />
           </Switch>
         </Router>
-      </div>
     </div>
   );
 }
